8c9bc4c4adf80c07280715b58c859d398c65c63e
Héctor <UO277303@uniovi.es>
2023-02-27 13:43:54 +0100

    SDI-2223-308-4.2-SpringBoot-Roles

--- src/main/java/com/uniovi/sdi2223308spring/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi2223308spring/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.uniovi.sdi2223308spring.controllers;
 
 import com.uniovi.sdi2223308spring.entities.User;
+import com.uniovi.sdi2223308spring.services.RolesService;
 import com.uniovi.sdi2223308spring.services.SecurityService;
 import com.uniovi.sdi2223308spring.services.UsersService;
 import com.uniovi.sdi2223308spring.validators.SignUpFormValidator;
@@ -21,6 +22,8 @@ public class UsersController {
     private SecurityService securityService;
     @Autowired
     private SignUpFormValidator signUpFormValidator;
+    @Autowired
+    private RolesService rolesService;
 
     @RequestMapping("/user/list")
     public String getListado(Model model) {
@@ -29,7 +32,7 @@ public class UsersController {
     }
     @RequestMapping(value = "/user/add")
     public String getUser(Model model) {
-        model.addAttribute("usersList", usersService.getUsers());
+        model.addAttribute("rolesList", rolesService.getRoles());
         return "user/add";
     }
     @RequestMapping(value = "/user/add", method = RequestMethod.POST)
@@ -70,7 +73,7 @@ public class UsersController {
         if (result.hasErrors()) {
             return "/signup";
         }
-
+        user.setRole(rolesService.getRoles()[0]);
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
